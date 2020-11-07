699b5579f6cc97444993b5a9271de641096cefd7
LososAlex <LososAlex@yandex.ru>
2020-11-07 18:02:24 +0500

    fix
    spelling correction
    fix bug with reciprocal numbers in affin decoder

--- Afin.py
+++ Afin.py
@@ -1,3 +1,6 @@
+import math
+
+
 def afin_encode(text, a, b):
     alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
                 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
@@ -24,20 +27,23 @@ def afin_decode(text, a, b):
     decode_text = []
     alt_a = 1
     ans = 1
-    while (alt_a * a) % 33 != 1:
-        if ans % a != 0:
-            ans += 33
-        else:
-            alt_a = int(ans / a)
-    for i in text:
-        i = list(i)
-        word = ''
-        for j in i:
-            if j.isalpha():
-                index = alphabet.index(j)
-                decode_index = ((index - b) * alt_a) % 33
-                word += alphabet[decode_index]
+    if math.gcd(a, 33) == 1:
+        while (alt_a * a) % 33 != 1:
+            if ans % a != 0:
+                ans += 33
             else:
-                word += j
-        decode_text.append(word)
-    return decode_text
+                alt_a = int(ans / a)
+        for i in text:
+            i = list(i)
+            word = ''
+            for j in i:
+                if j.isalpha():
+                    index = alphabet.index(j)
+                    decode_index = ((index - b) * alt_a) % 33
+                    word += alphabet[decode_index]
+                else:
+                    word += j
+            decode_text.append(word)
+        return decode_text
+    else:
+        return 'Невозможно расшифровать, т.к. ключ а не взаимно обратен 33'.split()
